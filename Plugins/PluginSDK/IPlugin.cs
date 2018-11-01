823fcd7494de53f17dd2f5a6e4e0bdcbadc582fe
Yuki Takeya <yuki@gakkougurashi.net>
2018-11-01 17:11:08 +0800

    Add Empty Project

---
+++ Plugins/PluginSDK/IPlugin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yuki.Notepad.Plugin.SDK
+{
+    /// <summary>
+    /// 插件公共接口类
+    /// </summary>
+    public interface IPlugin
+    {
+        
+    }
+}
